7c9292eb23362e797d5f9fe20609f8ce90af4a6a
raki123 <rafael.kiesel@web.de>
2023-07-02 20:49:45 +0200

    Changed Annotated Decomposition.

--- src/annotated_decomposition.hpp
+++ src/annotated_decomposition.hpp
@@ -10,17 +10,19 @@ enum NodeType {
     JOIN
 };
 
-struct NodeAnnotation {
+struct AnnotatedNode {
   public:
+    size_t parent; // parent of the node or -1 if root
     NodeType type;
     Edge edge; // if LEAF or PATH_LIKE
     std::pair<size_t, size_t> children; // if JOIN the indices of the child nodes in the annotated decomposition
+    std::vector<vertex_t> bag;
 };
 
 
 // contains annotated nodes
 // path like nodes continuously from leaf to last node before join
 // join node occurs after both children nodes
-typedef std::vector<std::pair<NodeAnnotation, std::vector<vertex_t>>> AnnotatedDecomposition;
+typedef std::vector<AnnotatedNode> AnnotatedDecomposition;
 
 } // namespace
