0001b5fe956b9b46f86ba660c9a8b1b11848550c
raki123 <rafael.kiesel@web.de>
2023-07-02 20:57:10 +0200

    need parent after all

--- src/annotated_decomposition.hpp
+++ src/annotated_decomposition.hpp
@@ -12,6 +12,7 @@ enum NodeType {
 
 struct AnnotatedNode {
   public:
+    size_t parent; // parent of the node or -1 if root
     NodeType type;
     Edge edge; // if LEAF or PATH_LIKE
     std::pair<size_t, size_t> children; // if JOIN the indices of the child nodes in the annotated decomposition
