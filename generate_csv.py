494188871cf8c0c65a9492a6f20318aaed82aa2c
raki123 <rafael.kiesel@web.de>
2023-07-12 23:38:34 +0200

    updated csv script.

--- generate_csv.py
+++ generate_csv.py
@@ -2,12 +2,12 @@ import sys
 
 pw_tw_max_min_file = sys.argv[1]
 
-result = [["path_width", "tree_width", "join_sum", "max_length", "min_length", "length_diff"]]
+result = [["path_width", "tree_width", "join_sum", "max_length", "min_length", "#automorphisms", "length_diff"]]
 
 with open(pw_tw_max_min_file, "r") as stats:
     for line in stats:
-        result.append([int(v) for v in line.split()])
-        result[-1].append(result[-1][3] - result[-1][4])
+        result.append(line.split())
+        result[-1].append(int(result[-1][3]) - int(result[-1][4]))
 
 for path in sys.argv[2:]:
     with open(path, "r") as solver_stats:
@@ -15,9 +15,9 @@ for path in sys.argv[2:]:
         for i, line in enumerate(solver_stats):
             line = line.split()
             if len(line) < 2:
-                result[i + 1].append(-1)
+                result[i + 1].append("fail")
             else:
-                result[i + 1].append(int(line[1]))
+                result[i + 1].append(f"{line[2]}")
 with open("overall_stats.csv", "w") as out_file:
     for row in result:
         out_file.write(", ".join([str(val) for val in row]))
