b3e518fa53adbe88a6457b168e53495ba1488e63
Rafael Kiesel <rafael@flightkeys.com>
2023-07-15 12:26:59 +0000

    Adapted runscripts to makefile.

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@ NR_THREADS=1
 for j in $( seq 1 $NR_THREADS )
 do
     for (( i = $j-1; i < ${#FILES[@]}; i += $NR_THREADS )); do
-        if ! ( timeout 10 ./build/read < one_pair/${FILES[$i]} 2>/dev/null | xargs echo "$i $@"  )
+        if ! ( timeout 600 ./a.out < one_pair/${FILES[$i]} 2>/dev/null | xargs echo "$i $@"  )
         then
             echo "$i timeout"
         fi
     done &
 done
+
--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,12 @@
 FILES=(`ls all_pairs/`)
-NR_THREADS=4
+NR_THREADS=1
 for j in $( seq 1 $NR_THREADS )
 do
     for (( i = $j-1; i < ${#FILES[@]}; i += $NR_THREADS )); do
-        if ! ( timeout 100 ./build/read < all_pairs/${FILES[$i]} 2>/dev/null | xargs echo "$i $@"  )
+        if ! ( timeout 600 ./a.out < all_pairs/${FILES[$i]} 2>/dev/null | xargs echo "$i $@"  )
         then
             echo "$i timeout"
         fi
     done &
 done
+
